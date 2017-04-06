f7178759bcea0b055c1315fec450cb9bfaede99d
isiahmacs <isiah_macalalad@dlsu.edu.ph>
2017-04-07 00:28:51 +0800

    Updated Client Manager class
    Added deleteClient, getClient and updateClient

--- src/db/ClientManager.java
+++ src/db/ClientManager.java
@@ -40,8 +40,8 @@ public class ClientManager {
 	public boolean addClient(Client client) {
 		boolean result;
 		
-		String query = "INSERT INTO" + client.TABLE_NAME + " (" + client.CLIENT_ID + ", " 
-		+ client.CLIENT_NAME + ") VALUES ('" + client.getClientID() + "', '" + client.getName() + "')";
+		String query = "INSERT INTO" + Client.TABLE_NAME + " (" + Client.CLIENT_ID + ", " 
+		+ Client.CLIENT_NAME + ") VALUES ('" + client.getClientID() + "', '" + client.getName() + "')";
 		
 		DBConnection db = new DBConnection();
 		
@@ -54,5 +54,77 @@ public class ClientManager {
 		}
 		
 		return result;
+	}
+	
+	public boolean deleteClient(Client client) {
+		boolean result;
+		
+		String query = "DELETE FROM " + Client.TABLE_NAME + " WHERE " 
+						+ Client.CLIENT_ID + " = '" + client.getClientID() +"'";
+		
+		DBConnection db = new DBConnection();
+		
+		try {
+			result = db.editRow(query);
+		} finally {
+			if(db != null) {
+				db.close();
+			}
+		}
+		
+		return result;
+	}
+	
+	public boolean updateClient(Client client) {
+		boolean result;
+		
+		String query = "UPDATE " + Client.TABLE_NAME +
+					   "SET " + Client.CLIENT_ID + " = '" + client.getClientID()
+					   + "' WHERE " + Client.CLIENT_NAME + " = '" + client.getName() + "'";
+		
+		DBConnection db = new DBConnection();
+		
+		try {
+			result = db.editRow(query);
+		} finally {
+			if(db != null) {
+				db.close();
+			}
+		}
+		
+		return result;
+	}
+	
+	public Client getClient(int client_id, String name) {
+		
+		Client client = null;
+		ResultSet result;
+		
+		String query = "SELECET * FROM " + Client.TABLE_NAME + " WHERE " +
+						Client.CLIENT_ID + " = '" + client_id + "' AND " +
+						Client.CLIENT_NAME + " = '" + name + "'";
+		
+		DBConnect db = new DBConnection();
+		
+		try {
+			
+			db.select(query);
+			
+			result = db.getResult();
+			
+			result.next();
+			client = new Client();
+			client.setClientID(result.getInt(Client.CLIENT_ID));
+			client.setName(result.getString(Client.CLIENT_NAME));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(db != null) {
+				db.close();
+			}
+		}
+		
+		return client;
 	}
 }
