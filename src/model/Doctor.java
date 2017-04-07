35a2da16568dc28470f5bf611a55e5454c67ad0f
Jade Yu <jade_yu@dlsu.edu.ph>
2017-04-07 23:02:56 +0800

    removed appointments attribute
    On second thought, di na pala kailangan ng appointments sa Doctor class
    since malalaman naman sa Appointment kung sino yung doctor, and dapat sa
    CalendarModel yung pag-add and cancel.

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,15 +1,11 @@
 package model;
 
-
-import java.util.ArrayList;
-
 public class Doctor {
 	
 	public static int ctr = 0;
 	
 	private final int id;
 	private String name;
-	private ArrayList<Appointment> appointments;
 	
 	public Doctor(String n)
 	{
@@ -17,22 +13,6 @@ public class Doctor {
 		
 		this.id = ctr;
 		name = n;
-		appointments = new ArrayList<Appointment>();
-	}
-	
-	public void addAppointment(Appointment a)
-	{
-		appointments.add(a);
-	}
-	
-	public void cancelAppointment(int id)
-	{
-		for(int i = 0 ; i < appointments.size(); i++) {
-			//not sure this is already okay or if it should still check if the appointment is free (should that be here or the model?)
-			if(appointments.get(i).getID() == id) {
-				appointments.remove(i);
-			}
-		}
 	}
 	
 	public String getName() {
