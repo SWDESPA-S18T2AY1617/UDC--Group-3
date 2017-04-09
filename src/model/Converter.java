1c924eef2de1c912e0a83183b40341791a9b6cd6
DESKTOP-LPN246V\krizi <krizia_chiu@dlsu.edu.ph>
2017-04-10 03:26:48 +0800

    added toDB

--- src/model/Converter.java
+++ src/model/Converter.java
@@ -44,4 +44,35 @@ public class Converter {
 		Doctor doctor = ModelFactory.createDoctor(doctorDB.getDoctor_id(), doctorDB.getName());
 		return doctor;
 	}
+	
+	public static AppointmentDB toAppointmentDB(Appointment appointment) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		AppointmentDB appointmentDB = new AppointmentDB(appointment.getID(), sdf.format(appointment.getStart().getTime()), sdf.format(appointment.getEnd().getTime()), "", appointment.getDoctor().getID());
+		if(appointment.getClient() != null) {
+			appointmentDB.setClient_id(appointment.getClient().getID());
+		}
+		/*
+		if(appointmentDB.getType().equals("Client")){
+			ClientManager CM = new ClientManager();
+			Client client = toClient(CM.getClient(appointmentDB.getClient_id()));
+			appointment.setClient(client);
+		}
+		else if(appointmentDB.getType().equals("Doctor")){
+			DoctorManager DM = new DoctorManager();
+			Doctor doctor = toDoctor(DM.getDoctor(appointmentDB.getDoctor_id()));
+			appointment.setDoctor(doctor);		
+		}
+		*/
+		return appointmentDB;
+	}
+	
+	public static ClientDB toClientDB(Client client) {
+		ClientDB clientDB = new ClientDB(client.getID(), client.getName());
+		return clientDB;
+	}
+	
+	public static DoctorDB toDoctorDB(Doctor doctor) {
+		DoctorDB doctorDB = new DoctorDB(doctor.getID(), doctor.getName());
+		return doctorDB;
+	}
 }
