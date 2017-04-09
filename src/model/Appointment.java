dbb6ff19cf5ab656ae81300f2246cddd374d112b
DESKTOP-LPN246V\krizi <krizia_chiu@dlsu.edu.ph>
2017-04-09 21:05:56 +0800

    Added more constructors + added getters and setters

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -17,6 +17,31 @@ public class Appointment {
 		ctr++;
 		
 		this.id = ctr;
+		doctor = null;
+		client = null;
+		startTime = start;
+		endTime = end;
+	}
+	
+	public Appointment(Doctor doctor, Calendar start)
+	{
+		ctr++;
+		
+		this.id = ctr;
+		this.doctor = doctor;
+		client = null;
+		startTime = start;
+		endTime = (Calendar) startTime.clone();
+		
+		endTime.set(Calendar.MINUTE, endTime.get(Calendar.MINUTE) + 30);
+	}
+	
+	public Appointment(Doctor doctor, Calendar start, Calendar end)
+	{
+		ctr++;
+		
+		this.id = ctr;
+		this.doctor = doctor;
 		client = null;
 		startTime = start;
 		endTime = end;
@@ -47,33 +72,81 @@ public class Appointment {
 		
 		return temp.toString();
 	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
 
 	public int getID() {
 		return id;
 	}
 
+	public int getMonth() {
+		return startTime.get(Calendar.MONTH);
+	}
+
+	public int getDay() {
+		return startTime.get(Calendar.DAY_OF_MONTH);
+	}
+
+	public int getYear() {
+		return startTime.get(Calendar.YEAR);
+	}
+	
+	public int getStartHour() {
+		return startTime.get(Calendar.HOUR_OF_DAY);
+	}
+	
+	public int getStartMinute() {
+		return startTime.get(Calendar.MINUTE);
+	}
+	
+	public int getEndHour() {
+		return endTime.get(Calendar.HOUR_OF_DAY);
+	}
+	
+	public int getEndMinute() {
+		return endTime.get(Calendar.MINUTE);
+	}
+
+	public boolean isYear(int year) {
+		return getYear() == year;
+	}
+
+	public boolean isMonth(int month) {
+		return getMonth() == month;
+	}
+
+	public boolean isDay(int day) {
+		return getDay() == day;
+	}
+	
+	public boolean isSameDay(Calendar start, Calendar end) {
+		return startTime.compareTo(start) == 0 && endTime.compareTo(end) == 0;
+	}
+
+	public Calendar getStart() {
+		return (Calendar) startTime.clone();
+	}
+	
+	public Calendar getEnd() {
+		return (Calendar) endTime.clone();
+	}
+	
+	public boolean isTimeValid() {
+		return startTime.before(endTime);
+	}
+	
 	public Calendar getStartTime() {
 		return startTime;
 	}
 
-	public int getStartHour() {
-		return startTime.get(Calendar.HOUR_OF_DAY);
-	}
-
-	public int getStartMinute() {
-		return startTime.get(Calendar.MINUTE);
-	}
-
 	public Calendar getEndTime() {
 		return endTime;
-	}
-
-	public int getEndHour() {
-		return endTime.get(Calendar.HOUR_OF_DAY);
-	}
-
-	public int getEndMinute() {
-		return endTime.get(Calendar.MINUTE);
 	}
 
 	public void setStartTime(Calendar startTime) {
@@ -84,4 +157,11 @@ public class Appointment {
 		this.endTime = endTime;
 	}
 	
+	public boolean isAvailable() {
+		return client == null;
+	}
+	
+	public boolean isOverlapping(Appointment activity) {
+		return this.startTime.before(activity.getEnd()) && activity.getStart().before(this.endTime);
+	}
 }
