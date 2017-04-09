325901422593d839a36eeb6a68c99bc740534f3e
ClarkJasonKong <clark_kong@dlsu.edu.ph>
2017-04-09 16:14:01 +0800

    commented out non-functional methods for testing #3 PRAMIS LAST NA TOH

--- src/view/secretary/PanelBookingView.java
+++ src/view/secretary/PanelBookingView.java
@@ -199,7 +199,7 @@ public class PanelBookingView extends JPanel{
 	}
 	class btnSave_Action implements ActionListener{
 		public void actionPerformed (ActionEvent e){
-			vc.setAppointment();
+		//	vc.setAppointment();
 
 			bookDetails.setText("");
 			vc.setCalendarPanel();
