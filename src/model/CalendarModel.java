f8478a27bdacbc59b3edf525c64ec368a8d5360d
DESKTOP-LPN246V\krizi <krizia_chiu@dlsu.edu.ph>
2017-04-09 21:06:34 +0800

    added getAppointments method

--- src/model/CalendarModel.java
+++ src/model/CalendarModel.java
@@ -1,19 +1,55 @@
 package model;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 
 public class CalendarModel {
-	//private ArrayList<Appointment> appointments;
+	// private ArrayList<Appointment> appointments;
+	private ArrayList<Doctor> doctors;
 
 	public CalendarModel() {
-		
+		doctors = new ArrayList<>();
 	}
-	/*
-	public void sortAppointments() {
+
+	public void addDoctor(Doctor d) {
+		doctors.add(d);
+	}
+	
+	
+
+	public ArrayList<Appointment> getAppointments(int doctorId) {
+		ArrayList<Appointment> a = new ArrayList<>();
+		for (Doctor doctor : doctors) {
+			if (doctor.getID() == doctorId) {
+				Iterator<Appointment> iterator = doctor.getAppointments();
+				while (iterator.hasNext()) {
+					Appointment appointment = iterator.next();
+					a.add(appointment);
+				}
+				return a;
+			}
+		}
+
+		return a;
+	}
+
+	public ArrayList<Appointment> getAllAppointments() {
+		ArrayList<Appointment> a = new ArrayList<>();
+		for (Doctor doctor : doctors) {
+			Iterator<Appointment> iterator = doctor.getAppointments();
+			while (iterator.hasNext()) {
+				Appointment appointment = iterator.next();
+				a.add(appointment);
+			}
+		}
+
+		sortAppointments(a);
+		return a;
+	}
+
+	public void sortAppointments(ArrayList<Appointment> appointments) {
 		Collections.sort(appointments, new Comparator<Appointment>() {
 			public int compare(Appointment s1, Appointment s2) {
 				int n = s1.getYear() - s2.getYear();
@@ -29,5 +65,5 @@ public class CalendarModel {
 			}
 		});
 	}
-	*/
+
 }
